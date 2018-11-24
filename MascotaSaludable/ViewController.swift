e80afcd2e90d55bc85107c1d8afb93303c4c1cdf
armandoGutierrez <gutierrezjarr@gmail.com>
2018-11-23 21:39:28 -0800

    Diseno de login

--- MascotaSaludable/ViewController.swift
+++ MascotaSaludable/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUsuario: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pressAccessButton(_ sender: Any) {
+        
+        if txtUsuario.text == txtPassword.text {
+            let alertController = UIAlertController(title: "iOScreator", message:
+                "Hello, world!", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            
+            self.present(alertController, animated: true, completion: nil)        }
+    }
 
 }
 
